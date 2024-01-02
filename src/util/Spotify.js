4b3df250cffce71bb4acc0e44d1ce01c16ede87c
chifavz <chifavz@gmail.com>
2024-01-02 15:07:29 -0500

    revisted

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,4 +1,4 @@
-const clientId = process.env.REACT_APP_CLIENT_ID; // Insert client ID here.
+const clientId = '93f45bc4efcc46e1b793b076c151aa50'; // Insert client ID here.
 const redirectUri = 'http://localhost:3000/'; // Have to add this to your accepted Spotify redirect URIs on the Spotify API.
 let accessToken;
 
